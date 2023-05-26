468283b029447c7413eb97949502a611dfc52771
AlambrezFE <andre29flores.aga.nst@gmail.com>
2023-05-25 21:17:18 -0500

    conexion con digital ocean

--- php/conexion.php
+++ php/conexion.php
@@ -1,17 +1,18 @@
-<?php 
-$contrasena = "";
-$usuario = "root";
-$nombre_bd = "grupo";
+<?php
+$contrasena = "AVNS_XtsNKgJwDlH934mJ0-9";
+$usuario = "doadmin";
+$nombre_bd = "defaultdb";
+$host = "db-mysql-nyc1-61857-do-user-14089120-0.b.db.ondigitalocean.com";
+$port = 25060;
 
 try {
-	$bd = new PDO (
-		'mysql:host=localhost;
-		dbname='.$nombre_bd,
-		$usuario,
-		$contrasena,
-		array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
-	);
+    $bd = new PDO(
+        "mysql:host=$host;port=$port;dbname=$nombre_bd;sslmode=REQUIRED",
+        $usuario,
+        $contrasena,
+        array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
+    );
 } catch (Exception $e) {
-	echo "Problema con la conexion: ".$e->getMessage();
+    echo "Problema con la conexión: " . $e->getMessage();
 }
 ?>
